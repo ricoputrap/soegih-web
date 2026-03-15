f97fc299b9c868b674933068ab5fbeb72d87645d
Rico Putra Pradana <ricopradanaputra2198@gmail.com>
2026-03-15 14:34:36 +0700

    feat(auth): add auth context and route guard

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,12 +1,15 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { AuthProvider } from '../shared/context/auth-context'
 
 const queryClient = new QueryClient()
 
 export const Route = createRootRoute({
   component: () => (
     <QueryClientProvider client={queryClient}>
-      <Outlet />
+      <AuthProvider>
+        <Outlet />
+      </AuthProvider>
     </QueryClientProvider>
   ),
 })
